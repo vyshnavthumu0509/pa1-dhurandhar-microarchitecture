19cb41680bbfee011283bc0f750f1a52610c8dbc
vyshnavthumu0509 <vyshnavthumu0509@gmail.com>
2026-08-31 17:20:59 +0530

    unrolled four fold

--- task1/src/conv_unroll.cpp
+++ task1/src/conv_unroll.cpp
@@ -3,6 +3,30 @@
 
 void conv_unroll(const float* in, float* out, const float* ker,
                  int H, int W, int K) {
-    // TODO(student): replace this placeholder with your unrolled implementation.
-    conv_naive(in, out, ker, H, W, K);
+    const int p = K / 2;
+    const int in_stride = W + 2 * p;
+
+    for (int i = 0; i < H * W; ++i) out[i] = 0.0f;
+
+    for (int ky = 0; ky < K; ++ky) {
+        for (int kx = 0; kx < K; ++kx) {
+            const float kv = ker[ky * K + kx];
+            for (int oy = 0; oy < H; ++oy) {
+                const float* in_row  = &in[(oy + ky) * in_stride + kx];
+                float*       out_row = &out[oy * W];
+
+                int ox = 0;
+                const int W4 = W & ~3;  
+                for (; ox < W4; ox += 4) {
+                    out_row[ox+0] += in_row[ox+0] * kv;
+                    out_row[ox+1] += in_row[ox+1] * kv;
+                    out_row[ox+2] += in_row[ox+2] * kv;
+                    out_row[ox+3] += in_row[ox+3] * kv;
+                }
+                for (; ox < W; ++ox) {         
+                    out_row[ox] += in_row[ox] * kv;
+                }
+            }
+        }
+    }
 }
